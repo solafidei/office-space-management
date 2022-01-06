3e55f4ff275ee60cd8d83b5c7bf8178ba2abb9db
Solomon Razaq <souls1@live.co.za>
2022-01-06 12:41:42 +0200

    angular - feat - add staff functionality

--- src/app/staffs/staff-create/staff-create.component.ts
+++ src/app/staffs/staff-create/staff-create.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { MatDialog } from "@angular/material/dialog";
+import { Staff } from "../staff.model";
 import { StaffsService } from "../staffs.service";
 
 @Component({
@@ -8,28 +10,36 @@ import { StaffsService } from "../staffs.service";
 })
 export class StaffCreateComponent implements OnInit{
   @Input() public officeId;
+  @Input() public staff: Staff;
   isLinear = false;
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
 
-  constructor(private _formBuilder: FormBuilder, public StaffService: StaffsService) {}
+  constructor(private _formBuilder: FormBuilder, public StaffService: StaffsService, private diag: MatDialog) {}
 
   ngOnInit() {
     this.firstFormGroup = this._formBuilder.group({
-      firstNameFormCtrl: ['', Validators.required],
-      lastNameFormCtrl: ['', Validators.required],
+      firstNameFormCtrl: [this.staff?.firstName, Validators.required],
+      lastNameFormCtrl: [this.staff?.lastName, Validators.required],
     });
     this.secondFormGroup = this._formBuilder.group({
       avatarCtrl: ['', Validators.required]
     });
-    console.log(this.officeId);
+    console.log(this.staff);
   }
-  addStaff(){
+  saveStaff(){
     if (this.firstFormGroup.valid)
     {
       const firstName = this.firstFormGroup.controls['firstNameFormCtrl'].value;
-      const lastName = this.firstFormGroup.controls['firstNameFormCtrl'].value;
-      this.StaffService.addStaff(firstName, lastName, this.officeId);
+      const lastName = this.firstFormGroup.controls['lastNameFormCtrl'].value;
+      if (this.staff)
+      {
+        this.StaffService.updateStaff(this.staff._id, firstName, lastName, this.officeId);
+      }
+      else {
+        this.StaffService.addStaff(firstName, lastName, this.officeId);
+      }
+      this.diag.closeAll();
     }
   }
 }
